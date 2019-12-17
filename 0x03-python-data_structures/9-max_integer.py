3a161591c552c2addfedcec16a7577d4e7ebfa10
boumaiza-kais <1110@holbertonschool.com>
2019-12-17 20:47:09 +0100

    commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def a_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    a = my_list[0]
+    for i in my_list:
+        if a < i:
+            a = i
+    return a
