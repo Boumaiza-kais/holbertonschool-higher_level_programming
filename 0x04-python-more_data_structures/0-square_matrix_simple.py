b421e60d0afd46599382eb8aa76299b0db2db0fc
boumaiza-kais <1110@holbertonschool.com>
2019-12-20 23:57:42 +0100

    commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if (matrix):
-       new_matrix = []
+     new_matrix = []
        for i in range(len(matrix)):
            new_matrix.append(list(map(lambda x: x * x, matrix[i])))
         return new_matrix
