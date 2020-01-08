c3fe312b28915ee1677bdfa94a31e83c59e7f185
boumaiza-kais <1110@holbertonschool.com>
2020-01-08 16:35:27 +0100

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
-             print("{:d}" .format(i), end="")
+            print(my_list[i], end='')
         except IndexError:
             break
         count += 1
