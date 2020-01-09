b38e761f46bf93e5f096891525219ad74ae4fb72
boumaiza-kais <1110@holbertonschool.com>
2020-01-09 15:11:47 +0100

    commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Square that defines a square by: (based on 2-square.py)"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
