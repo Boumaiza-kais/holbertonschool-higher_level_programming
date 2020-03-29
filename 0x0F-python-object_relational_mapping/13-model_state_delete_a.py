7dda9c0001e8d9a906461d396f80cafef5684a5b
boumaiza-kais <1110@holbertonschool.com>
2020-03-29 22:05:28 +0100

    task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,7 +7,7 @@ import sqlalchemy
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
