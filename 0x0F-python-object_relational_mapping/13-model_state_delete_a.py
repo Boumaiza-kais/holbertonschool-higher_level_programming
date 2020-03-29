8447c28ac20ac9c61cc27932657910a58478f5ce
boumaiza-kais <1110@holbertonschool.com>
2020-03-29 20:36:59 +0100

    task13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,10 +11,13 @@ from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+                            argv[1], argv[2], argv[3]))
 
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-    for state in session.query(State).filter(State.name.like("%a%")):
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%'))
+    for state in states:
         session.delete(state)
     session.commit()
+    session.close()
